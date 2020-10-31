065df0286377e0fc94c45bc310dd78c675f794e6
Emelie Erdbeer <51999910+dr4k0nia@users.noreply.github.com>
2020-10-31 23:31:40 +0100

    Code improvements

--- Unscrambler/Features/LocalsToFieldRemover.cs
+++ Unscrambler/Features/LocalsToFieldRemover.cs
@@ -9,59 +9,52 @@ namespace Unscrambler.Features
     public class LocalsToFieldRemover : IFeature
     {
         private int _count;
-        private static readonly List<FieldDefinition> FieldsInModule = new List<FieldDefinition>();
+        private static readonly HashSet<FieldDefinition> FieldsInModule = new HashSet<FieldDefinition>();
 
         private static readonly Dictionary<FieldDefinition, CilLocalVariable> CreatedLocals =
             new Dictionary<FieldDefinition, CilLocalVariable>();
 
         public void Process( TypeDefinition type )
         {
+            // On first execution, grab fields from global constructor
+            if ( FieldsInModule.Count == 0 )
+            {
+                var globalType = type.Module.GetOrCreateModuleType();
+
+                foreach ( var field in globalType.Fields )
+                {
+                    // This assumes that the fields have no default value (which the forks Ive checked didnt have)
+                    if ( !field.IsStatic || field.IsPrivate || field.HasDefault )
+                        continue;
+                    FieldsInModule.Add( field );
+                }
+            }
+
             foreach ( var method in type.Methods.Where( m => m.CilMethodBody != null ) )
             {
-                // On first execution, grab fields from global constructor
-                if ( FieldsInModule.Count == 0 )
-                {
-                    var globalType = method.Module.GetOrCreateModuleType();
-                    if ( globalType.Fields == null ) return;
-
-                    foreach ( var field in globalType.Fields )
-                    {
-                        // This assumes that the fields have no default value (which the forks Ive checked didnt have)
-                        if ( !field.IsStatic || field.IsPrivate || field.HasDefault )
-                            continue;
-                        FieldsInModule.Add( field );
-                    }
-                }
-
                 var instr = method.CilMethodBody.Instructions;
                 for ( int i = 0; i < instr.Count; i++ )
                 {
-                    if ( instr[i].OpCode != CilOpCodes.Stsfld && instr[i].OpCode != CilOpCodes.Ldsfld &&
-                         instr[i].OpCode != CilOpCodes.Ldsflda )
+                    if ( instr[i].OpCode.OperandType != CilOperandType.InlineField )
                         continue;
                     if ( !( instr[i].Operand is FieldDefinition field ) || !FieldsInModule.Contains( field ) )
                         continue;
 
-                    var fieldToReplace = FieldsInModule.Find( f => f == field );
-                    CilLocalVariable local;
-
                     // Check if dictionary already contains a local for the field, if not create a new one and add it to the dictionary
-                    if ( !CreatedLocals.ContainsKey( fieldToReplace ) )
+                    if ( !CreatedLocals.TryGetValue( field, out var createdLocal ) )
                     {
-                        local = new CilLocalVariable( fieldToReplace.Signature.FieldType );
-                        method.CilMethodBody.LocalVariables.Add( local );
-                        CreatedLocals.Add( fieldToReplace, local );
+                        createdLocal = new CilLocalVariable( field.Signature.FieldType );
+                        method.CilMethodBody.LocalVariables.Add( createdLocal );
+                        CreatedLocals.Add( field, createdLocal );
                     }
 
                     // Get local from dictionary
-                    CreatedLocals.TryGetValue( fieldToReplace, out var createdLocal );
-                    local = createdLocal;
-
-                    instr[i].OpCode = GetOpCode( instr[i].OpCode );
-                    instr[i].Operand = local;
-                    instr.OptimizeMacros();
+                    instr[i].OpCode = GetOpCode( instr[i].OpCode.Code );
+                    instr[i].Operand = createdLocal;
                     _count++;
                 }
+
+                instr.OptimizeMacros();
             }
         }
 
@@ -81,14 +74,17 @@ namespace Unscrambler.Features
                 yield return new Summary( $"Removed {_count} Local to Field implementations", Logger.LogType.Success );
         }
 
-        private static CilOpCode GetOpCode( CilOpCode opcode )
+        private static CilOpCode GetOpCode( CilCode opcode )
         {
-            if ( opcode == CilOpCodes.Stsfld )
-                return CilOpCodes.Stloc;
-            if ( opcode == CilOpCodes.Ldsfld )
-                return CilOpCodes.Ldloc;
-            if ( opcode == CilOpCodes.Ldsflda )
-                return CilOpCodes.Ldloca;
+            switch ( opcode )
+            {
+                case CilCode.Stsfld:
+                    return CilOpCodes.Stloc;
+                case CilCode.Ldsfld:
+                    return CilOpCodes.Ldloc;
+                case CilCode.Ldsflda:
+                    return CilOpCodes.Ldloca;
+            }
 
             return CilOpCodes.Nop;
         }
